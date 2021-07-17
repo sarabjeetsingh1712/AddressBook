4cbfc2a0c165bca87e3122de1074beb10b853388
sarabjeetsingh1712 <sarabjeet.singh1712@yahoo.com>
2021-07-17 23:43:34 +1000

    Update run.sh

--- AddressBook/addressbook-service/run.sh
+++ AddressBook/addressbook-service/run.sh
@@ -1,5 +1,5 @@
 echo "Cleaning & Building Address Book Application.."
 mvn clean package
 
-docker build -t addressbookapp:1.0
+docker build -t addressbookapp:1.0 .
 docker run -p 8080:8080 -t addressbookapp:1.0
